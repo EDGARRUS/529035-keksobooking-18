59f8ac01bfde2ee169ab81506ae1dc8fa8a398b1
EDGARRUS <37205598+EDGARRUS@users.noreply.github.com>
2019-09-28 13:55:49 +0300

    Generate and add Pins on map

---
+++ js/main.js
@@ -0,0 +1,78 @@
+'use strict';
+
+function arrayRandElement(arr) {
+  var rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+}
+
+var generateData = function (counter) {
+  var type = ['palace', 'flat', 'house', 'bungalo'];
+  var title = ['Luxury Apart', 'Good flat', 'My dear home', 'Great place', 'Wonderful house', 'Gold palace', 'Very cool home', 'Cheap flat in center'];
+  var description = ['Прекрасное место для отдыха', 'Вы полюбите это место как свой дом', 'Очень дешево и сердито', 'Вы не заходите уезжать из моего дома', 'Приезжай сейчас и я дам скидку 10%!', 'Хочешь красиво жить? Бронируй мое жилье', 'Красивый вид из окон на море', 'Квартира в самом центре города!'];
+  var checkin = ['12:00', '13:00', '14:00'];
+  var checkout = ['12:00', '13:00', '14:00'];
+  var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  var objectsData = [];
+
+  for (var i = 1; i <= counter; i++) {
+
+    var pinHeight = 65;
+    var locationX = Math.floor(Math.random() * 1200) - pinHeight;
+    var locationY = Math.floor(Math.random() * 500) + 130 - pinHeight;
+
+    objectsData.push({
+      'author': {
+        'avatar': 'img/avatars/user0' + i + '.png'
+      },
+      'offer': {
+        'title': arrayRandElement(title),
+        'address': locationX + ', ' + locationY,
+        'price': Math.floor(Math.random() * 10000),
+        'type': arrayRandElement(type),
+        'rooms': Math.floor(Math.random() * 10),
+        'guests': Math.floor(Math.random() * 10),
+        'checkin': arrayRandElement(checkin),
+        'checkout': arrayRandElement(checkout),
+        'features': arrayRandElement(features),
+        'description': arrayRandElement(description),
+        'photos': 'http://o0.github.io/assets/images/tokyo/hotel' + counter + '.jpg'
+      },
+      'location': {
+        'x': locationX,
+        'y': locationY
+      }
+    });
+  }
+
+  return objectsData;
+};
+
+var allObjects = generateData(8);
+
+var pinsList = document.querySelector('.map');
+pinsList.classList.remove('map--faded');
+
+var pinTemplate = document.getElementById('pin')
+  .content
+  .querySelector('.map__pin');
+
+var renderPin = function (pinData) {
+  var pinElement = pinTemplate.cloneNode(true);
+  var pinImage = pinElement.querySelector('img');
+  pinImage.setAttribute('alt', pinData.offer.title);
+  pinImage.setAttribute('src', pinData.author.avatar);
+  pinElement.style.left = pinData.location.x + 'px';
+  pinElement.style.top = pinData.location.y + 'px';
+
+  return pinElement;
+};
+
+var addPins = function (pinsArray) {
+  var pinFragment = document.createDocumentFragment();
+  for (var i = 0; i < pinsArray.length; i++) {
+    pinFragment.appendChild(renderPin(pinsArray[i]));
+  }
+  pinsList.appendChild(pinFragment);
+};
+
+addPins(allObjects);
